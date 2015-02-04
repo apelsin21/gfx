f30c7dcbf39620a7ffb6a61d525ab20261b67c0b
drmugg123 <drmugg123@protonmail.ch>
2015-02-04 13:56:55 +0100

    implemeted key checking for SDL2 in a pretty good way

--- src/gfx/sdl2keys.cpp
+++ src/gfx/sdl2keys.cpp
@@ -4,6 +4,85 @@ SDL_Scancode gfx::convertKeyToSDLScancode(gfx::KEYBOARD_KEY gfxKey) {
     SDL_Scancode sdlKey = SDL_SCANCODE_UNKNOWN;
 
     switch(gfxKey) {
+        case gfx::KEYBOARD_KEY::KEY_A:
+            sdlKey = SDL_SCANCODE_A;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_B:
+            sdlKey = SDL_SCANCODE_B;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_C:
+            sdlKey = SDL_SCANCODE_C;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_D:
+            sdlKey = SDL_SCANCODE_D;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_E:
+            sdlKey = SDL_SCANCODE_E;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_F:
+            sdlKey = SDL_SCANCODE_F;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_G:
+            sdlKey = SDL_SCANCODE_G;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_H:
+            sdlKey = SDL_SCANCODE_H;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_I:
+            sdlKey = SDL_SCANCODE_I;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_J:
+            sdlKey = SDL_SCANCODE_J;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_K:
+            sdlKey = SDL_SCANCODE_K;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_L:
+            sdlKey = SDL_SCANCODE_L;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_M:
+            sdlKey = SDL_SCANCODE_M;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_N:
+            sdlKey = SDL_SCANCODE_N;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_O:
+            sdlKey = SDL_SCANCODE_O;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_P:
+            sdlKey = SDL_SCANCODE_P;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_Q:
+            sdlKey = SDL_SCANCODE_Q;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_R:
+            sdlKey = SDL_SCANCODE_R;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_S:
+            sdlKey = SDL_SCANCODE_S;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_T:
+            sdlKey = SDL_SCANCODE_T;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_U:
+            sdlKey = SDL_SCANCODE_U;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_V:
+            sdlKey = SDL_SCANCODE_V;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_W:
+            sdlKey = SDL_SCANCODE_W;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_X:
+            sdlKey = SDL_SCANCODE_X;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_Y:
+            sdlKey = SDL_SCANCODE_Y;
+            break;
+        case gfx::KEYBOARD_KEY::KEY_Z:
+            sdlKey = SDL_SCANCODE_Z;
+            break;
+        
         case gfx::KEYBOARD_KEY::KEY_ESCAPE:
             sdlKey = SDL_SCANCODE_ESCAPE;
             break;
